6562a11b67f35953b0dced1c78c9095472d6a94b
Pablo Suárez <104940521+psuarezdev@users.noreply.github.com>
2025-02-20 11:35:26 +0000

    Update instructor-profile.tsx

--- src/app/(main)/instructors/[id]/_components/instructor-profile.tsx
+++ src/app/(main)/instructors/[id]/_components/instructor-profile.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRouter, usePathname } from 'next/navigation';
-import type { Course, Category, User } from '@prisma/client';
+import type { Course, Category, User, Rating } from '@prisma/client';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
@@ -16,17 +16,31 @@ import CourseCard from '@/components/course-card';
 import { Button } from '@/components/ui/button';
 import { Github, LinkedIn } from '@/components/icons';
 import { Globe } from 'lucide-react';
+import { UploadPaths } from '@/lib/config';
+import RatingStars from '@/components/rating-stars';
 
 interface InstructorProfileProps {
   instructor: Omit<User, 'password'>;
   courses: (Course & { category: Category })[];
   currentPage: number;
   totalPages: number;
+  ratings: Rating[];
 }
 
-export default function InstructorProfile({ instructor, courses, currentPage, totalPages }: InstructorProfileProps) {
+export default function InstructorProfile({ instructor, courses, ratings, currentPage, totalPages }: InstructorProfileProps) {
   const router = useRouter();
   const pathname = usePathname();
+
+  let ratingGroups: { [key: string]: Rating[] } = {};
+
+  Array.from({ length: 5 }).forEach((_, index) => {
+    ratingGroups[`${index + 1}`] = [];
+  });
+
+  ratingGroups = {
+    ...ratingGroups,
+    ...Object.groupBy(ratings, item => Math.trunc(item.rating))
+  };
 
   const handlePageChange = (page: number) => {
     router.push(`${pathname}?page=${page}`);
@@ -34,76 +48,100 @@ export default function InstructorProfile({ instructor, courses, currentPage, to
 
   return (
     <div className="grid gap-6 md:grid-cols-3">
-      <Card className="md:col-span-1 flex flex-col items-center h-fit">
-        <CardHeader>
-          <Avatar className="w-24 h-24 mx-auto">
-            <AvatarImage 
-              src={instructor.imageUrl ?? undefined} 
-              alt="Avatar" 
-            />
-            <AvatarFallback>
-              {instructor.firstName.charAt(0)}{instructor.lastName.charAt(0)}
-            </AvatarFallback>
-          </Avatar>
-          <CardTitle className="text-center">{instructor.firstName} {instructor.lastName}</CardTitle>
-          <CardDescription className="text-center">{instructor.email}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="text-muted-foreground">
-            {instructor.website && (
-              <Button
-                variant="ghost"
-                size="icon"
-                asChild
-              >
-                <a
-                  href={instructor.website}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title="Portfolio del instructor"
+      <div className="md:col-span-1">
+        <Card className="flex flex-col items-center h-fit mb-5">
+          <CardHeader>
+            <Avatar className="w-24 h-24 mx-auto">
+              <AvatarImage
+                src={instructor.avatar ? `/api/uploads/${UploadPaths.Avatars}/${instructor.avatar}` : undefined}
+                alt="Avatar"
+              />
+              <AvatarFallback>
+                {instructor.firstName.charAt(0)}{instructor.lastName.charAt(0)}
+              </AvatarFallback>
+            </Avatar>
+            <CardTitle className="text-center">{instructor.firstName} {instructor.lastName}</CardTitle>
+            <CardDescription className="text-center">{instructor.email}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="text-muted-foreground">
+              {instructor.website && (
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  asChild
                 >
-                  <Globe className="w-10 h-10" />
-                  <span className="sr-only">Portfolio del instructor</span>
-                </a>
-              </Button>
-            )}
-            {instructor.github && (
-              <Button
-                variant="ghost"
-                size="icon"
-                asChild
-              >
-                <a
-                  href={instructor.github}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title="GitHub del instructor"
+                  <a
+                    href={instructor.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Portfolio del instructor"
+                  >
+                    <Globe className="w-10 h-10" />
+                    <span className="sr-only">Portfolio del instructor</span>
+                  </a>
+                </Button>
+              )}
+              {instructor.github && (
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  asChild
                 >
-                  <Github className="w-10 h-10" />
-                  <span className="sr-only">GitHub del instructor</span>
-                </a>
-              </Button>
-            )}
-            {instructor.linkedin && (
-              <Button
-                variant="ghost"
-                size="icon"
-                asChild
-              >
-                <a
-                  href={instructor.linkedin}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title="LinkedIn del instructor"
+                  <a
+                    href={instructor.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="GitHub del instructor"
+                  >
+                    <Github className="w-10 h-10" />
+                    <span className="sr-only">GitHub del instructor</span>
+                  </a>
+                </Button>
+              )}
+              {instructor.linkedin && (
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  asChild
                 >
-                  <LinkedIn className="w-10 h-10" />
-                  <span className="sr-only">LinkedIn del instructor</span>
-                </a>
-              </Button>
-            )}
-          </div>
-        </CardContent>
-      </Card>
+                  <a
+                    href={instructor.linkedin}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="LinkedIn del instructor"
+                  >
+                    <LinkedIn className="w-10 h-10" />
+                    <span className="sr-only">LinkedIn del instructor</span>
+                  </a>
+                </Button>
+              )}
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="flex flex-col items-center h-fit">
+          <CardHeader>
+            <CardTitle className="text-center">Valoración general</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-muted-foreground">
+              {Object.keys(ratingGroups)
+                .sort((a, b) => Number(b) - Number(a))
+                .map(ratingGroupKey => (
+                  <div 
+                    key={`rating-group-${ratingGroupKey}`}
+                    className="flex items-center gap-1.5"
+                  >
+                    <RatingStars averageRating={Number(ratingGroupKey)} />
+                    <span className="text-sm font-medium ml-1">
+                      {ratingGroups[ratingGroupKey].length}
+                    </span>
+                  </div>
+                ))}
+            </div>
+          </CardContent>
+        </Card>
+      </div>
       <div className="md:col-span-2">
         <h2 className="text-2xl font-bold mb-4">Cursos del profesor</h2>
         <div className="grid gap-4 grid-cols-2 md:grid-cols-3">
